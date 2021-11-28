c7dc8e265022d70b6e09fa4a8c17b7e38217cf7f
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-28 20:46:47 +0100

    Cremos la clase "tablero" Y añadimos su funcion principal

--- Laberinto.py
+++ Laberinto.py
@@ -2,3 +2,8 @@ laberinto = []   # Definimos laberinto como una lista vacia
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))   
 # Creamos las cordenadas de los futuros muros del laberinto
 
+class tablero:
+        def __init__(self):
+                for i in range (5):
+                        laberinto.append(([' ', ' ', ' ', ' ', ' ']))
+                return laberinto
