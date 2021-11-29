18bacc10bc1776e0d6bbbcdf65a23f34a7691040
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-29 23:28:34 +0100

    Solucion errores

--- Laberinto.py
+++ Laberinto.py
@@ -4,10 +4,10 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 
 def crear_laberinto():
         for _ in range (5):
-                laberinto.append((([' ']) + '\n')*5)   # Creamos el laberinto sin muros,
+                laberinto.append(([' '])*5)  # Creamos el laberinto sin muros,
         for i in muro:                                 # con salto de linea incluido
-                laberinto (i[0])(i[1]) = 'X'   # De esta manera cada coordenada del                                    
-        return laberinto                       # muro sera una 'X' en el laberinto
+                laberinto[int(i[0])][int(i[1])] = 'X'   # De esta manera cada coordenada del                                    
+        return laberinto                                # muro sera una 'X' en el laberinto
 
 ole_ole = crear_laberinto()
 print (ole_ole)
