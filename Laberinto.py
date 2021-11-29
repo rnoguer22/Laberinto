e6d644a8910304efc9ba5f366fd0ce3a7a9a8b30
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-29 23:05:45 +0100

    Introducimos salto de linea en la lista del laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -3,9 +3,11 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 # Creamos las cordenadas de los futuros muros del laberinto
 
 def crear_laberinto():
-        for i in range (5):
-                laberinto.append(([' '])*5)   # Creamos el laberinto sin muros
-        return laberinto
+        for _ in range (5):
+                laberinto.append((([' ']) + '\n')*5)   # Creamos el laberinto sin muros,
+        for i in muro:                                 # con salto de linea incluido
+                laberinto (i[0])(i[1]) = 'X'   # De esta manera cada coordenada del                                    
+        return laberinto                       # muro sera una 'X' en el laberinto
 
-
-print (laberinto)
+ole_ole = crear_laberinto()
+print (ole_ole)
