4ce6c85458e0d571910bd52fe65862e7c67cd3eb
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-29 23:35:29 +0100

    Salto de linea

--- Laberinto.py
+++ Laberinto.py
@@ -4,10 +4,12 @@ muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4
 
 def crear_laberinto():
         for _ in range (5):
-                laberinto.append(([' '])*5)  # Creamos el laberinto sin muros,
-        for i in muro:                                 # con salto de linea incluido
+                laberinto.append(([' '])*5)  # Creamos el laberinto sin muros
+        for i in muro:                                 
                 laberinto[int(i[0])][int(i[1])] = 'X'   # De esta manera cada coordenada del                                    
         return laberinto                                # muro sera una 'X' en el laberinto
 
-ole_ole = crear_laberinto()
-print (ole_ole)
+ole_ole = crear_laberinto()   # Llamamos a la funcion
+
+for i in range (5):
+        print (ole_ole[i])   # Printamos de esta manera para obtener saltos de linea
