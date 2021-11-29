1809b6361c46b107d3ae22dc6df265755fbcd204
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-11-29 22:34:26 +0100

    definimos la funcion "crear_laberinto"

--- Laberinto.py
+++ Laberinto.py
@@ -1,11 +1,11 @@
-from random import randrange
-
-
 laberinto = []   # Definimos el futuro laberinto como una lista vacia
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))   
 # Creamos las cordenadas de los futuros muros del laberinto
 
-for i in range (5):
-        laberinto.append(([' '])*5)   # Creamos el laberinto sin muros
+def crear_laberinto():
+        for i in range (5):
+                laberinto.append(([' '])*5)   # Creamos el laberinto sin muros
+        return laberinto
+
 
 print (laberinto)
