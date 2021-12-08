413677f4bddf4b286daa334944e20ff6d52ed7ed
rnoguer22 <91721762+rnoguer22@users.noreply.github.com>
2021-12-08 13:26:46 +0100

    Completamos ka funcion "movimientos"

--- Laberinto.py
+++ Laberinto.py
@@ -13,7 +13,37 @@ for i in range (5):
 
 def movimientos ():
         movements = []
-        for i in laberinto:
-                if i != 'X':
-                             
+        m = 0
+        n = 0
+        k = 0
+        l = 0
+        while laberinto[m][n] != 'S':
+                if k <= m < 4 and laberinto[m][n+1] != 'X':
+                        movements.append("Derecha")
+                        print (movements)
+                        m += 1
+                        k = m - 1
+                        l = n
+                elif 0 < m < k and laberinto[m][n-1] != 'X':
+                        movements.append("Izquierda")
+                        print(movements)
+                        m -= 1
+                        k = m + 1
+                        l = n
+                elif 1 <= n < 4 and laberinto[m+1][n] != 'X':
+                        movements.append("Abajo")
+                        print (movements)
+                        n += 1
+                        l = n + 1
+                        k = m
+                elif 0 < n < 1 and laberinto[m-1][n] != 'X':
+                        movements.append("Arriba")
+                        print (movements)
+                        n -= 1
+                        l = n + 1
+                        k = m
+                elif m > 4 or n > 4:
+                        break
         return movements
+
+print (movimientos())
